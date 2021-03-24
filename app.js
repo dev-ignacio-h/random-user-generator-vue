d1cf43153d9d8a4cf3792263f0c1f1243507cd9a
Ignacio <dev.ignacio.h@gmail.com>
2021-03-24 13:01:54 -0300

    end

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const app = Vue.createApp({
-  template: '<h1>Hello World</h1>'
-})
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api')
+      const {results} = await res.json()
+      
+      const {name, email, gender, picture} = results[0]
 
-app.mount('#app')
+      this.firstName = name.first;
+      this.lastName = name.last;
+      this.email = email;
+      this.gender = gender;
+      this.picture = picture.large
+    }
+  }
+});
+
+app.mount('#app');
